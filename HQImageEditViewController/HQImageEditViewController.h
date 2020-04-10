33fea341322ab8a5f8c3da8e33bdc6d634c04507
Aimin <aimin987@163.com>
2020-04-10 14:25:12 +0800

    Initial Commit

---
+++ HQImageEditViewController/HQImageEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  HQImageEditViewController.h
+//  HQImageEditViewController
+//
+//  Created by aimin on 2020/4/10.
+//  Copyright © 2020 aimin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HQImageEditViewController : NSObject
+
+@end
---
+++ HQImageEditViewController/HQImageEditViewController.m
@@ -0,0 +1,13 @@
+//
+//  HQImageEditViewController.m
+//  HQImageEditViewController
+//
+//  Created by aimin on 2020/4/10.
+//  Copyright © 2020 aimin. All rights reserved.
+//
+
+#import "HQImageEditViewController.h"
+
+@implementation HQImageEditViewController
+
+@end
